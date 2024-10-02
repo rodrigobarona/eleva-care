5f22484910e6192f644fb9b0082a713480e96ed6
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2024-10-02 00:18:32 -0700

    update rules and disable two ANY

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,17 @@
 module.exports = {
   extends: [
-    'next',
-    'next/core-web-vitals',
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
+    "next",
+    "next/core-web-vitals",
+    "next/typescript",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ["react", "@typescript-eslint"],
   parserOptions: {
     ecmaVersion: 2021,
-    sourceType: 'module',
+    sourceType: "module",
   },
   rules: {
     // Add your custom ESLint rules here
--- components/molecules/Header.tsx
+++ components/molecules/Header.tsx
@@ -1,21 +1,22 @@
-import React, { useState, useEffect } from 'react'; // Added useState import
-import Link from 'next/link'; // Adjusted import to default export
-import { Button } from '@/components/ui/button';
+import React, { useState, useEffect } from "react"; // Added useState import
+import Link from "next/link"; // Adjusted import to default export
+import { Button } from "@/components/ui/button";
 
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
-import { Globe, ChevronDown } from 'lucide-react';
-import Image from 'next/image'; // Ensure correct import for Image component
+} from "@/components/ui/dropdown-menu";
+import { Globe, ChevronDown } from "lucide-react";
+import Image from "next/image"; // Ensure correct import for Image component
 
-type Language = 'en' | 'pt' | 'es'; // Use 'type' to import as a type
+type Language = "en" | "pt" | "es"; // Use 'type' to import as a type
 
 // Define the props type for Header
 type HeaderProps = {
-  t: string; // Adjust this type as needed
+  // eslint-disable-next-line
+  t: any;
   setLang: (lang: Language) => void; // Ensure setLang accepts Language type
 };
 
@@ -26,16 +27,21 @@ const Header: React.FC<HeaderProps> = ({ t, setLang }) => {
       setIsScrolled(window.scrollY > 0); // Set isScrolled based on scroll position
     };
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll); // Cleanup
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll); // Cleanup
   }, []);
   return (
     <header
       className={`fixed z-50 flex h-16 w-full items-center border-b border-[#0d6c70]/10 px-4 transition-colors lg:px-6 ${
-        isScrolled ? 'backdrop-blur supports-[backdrop-filter]:bg-white/60' : 'bg-transparent'
+        isScrolled
+          ? "backdrop-blur supports-[backdrop-filter]:bg-white/60"
+          : "bg-transparent"
       }`}
     >
-      <Link className="flex items-center justify-center" href="https://eleva.care">
+      <Link
+        className="flex items-center justify-center"
+        href="https://eleva.care"
+      >
         <Image
           src="/img/eleva-care-logo.svg"
           alt="Eleva Care"
@@ -72,16 +78,26 @@ const Header: React.FC<HeaderProps> = ({ t, setLang }) => {
         </Link>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="sm" className="text-[#576d69] hover:text-[#0d6c70]">
+            <Button
+              variant="ghost"
+              size="sm"
+              className="text-[#576d69] hover:text-[#0d6c70]"
+            >
               <Globe className="mr-2 h-5 w-5" />
               {t.language}
               <ChevronDown className="ml-2 h-4 w-4" />
             </Button>
           </DropdownMenuTrigger>
           <DropdownMenuContent align="end">
-            <DropdownMenuItem onClick={() => setLang('en')}>English</DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setLang('pt')}>Português</DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setLang('es')}>Español</DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setLang("en")}>
+              English
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setLang("pt")}>
+              Português
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setLang("es")}>
+              Español
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </nav>
