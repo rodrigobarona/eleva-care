4831884e70953b4c896464df6ec89b50ab27fc94
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2024-10-07 01:50:14 -0700

    change design image

--- components/molecules/ApproachSection.tsx
+++ components/molecules/ApproachSection.tsx
@@ -13,31 +13,33 @@ const ApproachSection: React.FC<ApproachSectionProps> = ({ title, items }) => {
     <FadeInSection>
       <section
         id="approach"
-        className="mx-2 rounded-5xl bg-[linear-gradient(145deg,var(--tw-gradient-stops))] from-elevaHighlight-yellow from-[28%] via-elevaHighlight-red via-[70%] to-elevaHighlight-purple py-10 lg:bg-[linear-gradient(115deg,var(--tw-gradient-stops))] lg:py-48"
+        className="mx-2 mt-24 rounded-5xl bg-[linear-gradient(145deg,var(--tw-gradient-stops))] from-elevaHighlight-yellow from-[28%] via-elevaHighlight-red via-[70%] to-elevaHighlight-purple py-10 lg:mt-32 lg:bg-[linear-gradient(115deg,var(--tw-gradient-stops))] lg:pb-32 lg:pt-20"
       >
         <div className="mx-auto max-w-2xl lg:max-w-7xl">
           <div className="grid grid-flow-row-dense grid-cols-1 gap-8 lg:grid-cols-12">
-            <div className="col-span-12 -m-6 aspect-square rounded-4xl bg-elevaNeutral-100/15 shadow-[inset_0_0_2px_1px_#ffffff4d] ring-1 ring-elevaNeutral-900/5 max-lg:mx-auto max-lg:max-w-xs lg:col-span-5 lg:-m-10">
-              <div className="rounded-4xl p-2 shadow-md shadow-elevaNeutral-900/5">
-                <div className="overflow-hidden rounded-3xl shadow-2xl outline outline-1 -outline-offset-1 outline-elevaNeutral-900/10">
-                  <Image
-                    alt=""
-                    src="/img/Three-Women-Posing-Photo.jpg"
-                    width={1920}
-                    height={1280}
-                    className="aspect-square w-full object-cover"
-                  />
+            <div className="col-span-12 -mt-24 lg:col-span-5 lg:-mt-52">
+              <div className="-m-4 aspect-[3/4] rounded-4xl bg-elevaNeutral-100/15 shadow-[inset_0_0_2px_1px_#ffffff4d] ring-1 ring-elevaNeutral-900/5 max-lg:mx-auto max-lg:max-w-xs lg:-m-10">
+                <div className="rounded-4xl p-2 shadow-md shadow-elevaNeutral-900/5">
+                  <div className="overflow-hidden rounded-3xl shadow-2xl outline outline-1 -outline-offset-1 outline-elevaNeutral-900/10">
+                    <Image
+                      alt=""
+                      src="/img/Three-Women-Posing-Photo.jpg"
+                      width={1920}
+                      height={1280}
+                      className="aspect-[3/4] w-full object-cover"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
 
             <div className="col-span-12 pl-6 lg:col-span-7 lg:pl-16">
               <div>
-                <h2 className="mt-4 text-pretty font-serif text-3xl font-light tracking-tighter text-elevaNeutral-100 data-[dark]:text-elevaNeutral-100 lg:text-6xl">
+                <h2 className="text-pretty font-serif text-3xl font-light tracking-tighter text-elevaNeutral-100 data-[dark]:text-elevaNeutral-100 lg:text-6xl">
                   {title}
                 </h2>
               </div>
-              <ol className="mx-auto mt-8 list-none text-elevaNeutral-100 marker:text-[#40514e]">
+              <ol className="mx-auto mt-6 list-none text-elevaNeutral-100 marker:text-[#40514e] lg:mt-8">
                 {items.map((item, index) => (
                   <li
                     key={index}
