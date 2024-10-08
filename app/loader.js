a8a56e70d5047476648cc2de2edff788b1d632ca
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2024-10-08 16:12:31 -0700

    fix and add Q

--- app/loader.js
+++ app/loader.js
@@ -1,8 +1,9 @@
-export default function imgixLoader = ({ src, width, quality }) => {
-    const url = new URL(`https://eleva-care-preview.imgix.net/${src}`)
-    const params = url.searchParams
-    params.set("auto", params.getAll("auto").join(",") || "format")
-    params.set("fit", params.get("fit") || "max")
-    params.set("w", params.get("w") || width.toString())
-    return url.href
-  }
+export default function imgixLoader({ src, width, quality }) {
+  const url = new URL(`https://eleva-care-preview.imgix.net${src}`);
+  const params = url.searchParams;
+  params.set('auto', params.getAll('auto').join(',') || 'format');
+  params.set('fit', params.get('fit') || 'max');
+  params.set('w', params.get('w') || width.toString());
+  params.set('q', quality.toString());
+  return url.href;
+}
