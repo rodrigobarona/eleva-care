3d6a04079666a4f5993c87e7a8c78120617cab12
Rodrigo Barona <rodrigo.barona@wundermanthompson.com>
2024-10-08 19:20:46 -0700

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,16 +1,11 @@
 const IMGIX_URL = process.env.NEXT_PUBLIC_IMGIX_URL;
 
-export default function imgixLoader({ src, width, quality = 75 }) {
+export default function imgixLoader({ src, width, quality }) {
   const url = new URL(`${IMGIX_URL}${src}`);
   const params = url.searchParams;
-
-  // Ensure quality is a valid number before using it
-  if (quality !== undefined) {
-    params.set('q', quality.toString());
-  }
-
   params.set('auto', params.getAll('auto').join(',') || 'format');
   params.set('fit', params.get('fit') || 'max');
   params.set('w', params.get('w') || width.toString());
+  params.set('q', (quality || 50).toString());
   return url.href;
 }
